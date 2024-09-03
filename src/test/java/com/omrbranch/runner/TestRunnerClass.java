38b00fb2729c1d79b6d6adc9207abc071f5e31be
Priyangathoughts <priyangas0595@gmail.com>
2024-09-04 02:12:11 +0530

    USA tymZone

--- src/test/java/com/omrbranch/runner/TestRunnerClass.java
+++ src/test/java/com/omrbranch/runner/TestRunnerClass.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith (Cucumber.class)
-@CucumberOptions(tags="@cancelbooking and ChangeBooking",features={"src\\main\\resources\\Features\\TC1_Login.feature","src\\main\\resources\\Features\\TC2_ExploreHotel.feature","src\\main\\resources\\Features\\TC3_SelectHotel.feature","src\\main\\resources\\Features\\TC4_BookHotel.feature","src\\main\\resources\\Features\\TC5_changeBooking.feature","src\\main\\resources\\Features\\TC6_CancelBooking.feature"} ,glue="com.omrbranch.stepdefinition")
+@CucumberOptions(tags="@CancelBooking and ChangeBooking",features={"src\\main\\resources\\Features\\TC1_Login.feature","src\\main\\resources\\Features\\TC2_ExploreHotel.feature","src\\main\\resources\\Features\\TC3_SelectHotel.feature","src\\main\\resources\\Features\\TC4_BookHotel.feature","src\\main\\resources\\Features\\TC5_changeBooking.feature","src\\main\\resources\\Features\\TC6_CancelBooking.feature"} ,glue="com.omrbranch.stepdefinition")
 public class TestRunnerClass {
 
 	
